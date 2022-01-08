2e45dc993f31f92c403476885cb86fe05ad7dd56
Jakub <jakubwolak.123@gmail.com>
2022-01-08 16:44:38 +0100

    add frontend

---
+++ frontend/src/components/header/HeaderDesktop.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <q-header elevated class="bg-transparent">
+    <q-toolbar>
+      <q-toolbar-title class="text-black">
+        Quasar App
+      </q-toolbar-title>
+
+      <q-tabs align="left" class="text-cyan-10">
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/page3" label="Page Three" />
+      </q-tabs>
+    </q-toolbar>
+  </q-header>
+</template>
---
+++ frontend/src/components/header/HeaderMobile.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  toggleDrawer: {
+    type: Function,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <q-header bordered class="bg-transparent">
+    <q-toolbar class="q-pl-lg q-pr-md q-py-sm">
+      <q-avatar square>
+        <q-img src="../../assets/matic_logo.svg" style="width: 50px; max-height: 50px;"/>
+      </q-avatar>
+      <q-toolbar-title>
+        <h1 class="text-h6 text-weight-bold text-primary">Matic Faucet</h1>
+      </q-toolbar-title>
+
+      <q-btn
+        flat
+        round
+        size="md"
+        aria-label="Menu"
+        class="text-cyan-10"
+        @click="props.toggleDrawer"
+      >
+          <q-icon name="las la-bars" />
+      </q-btn>
+    </q-toolbar>
+  </q-header>
+</template>
